5a4f74eb5bd6ec0e0fd425e115afd607dde97568
Susan Winters <susanwinters214@gmail.com>
2023-02-17 12:18:49 -0600

    created a few cars

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+       
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+        }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();  // the constructor
+            myCar.Make = "Chevy";
+            myCar.Model = "Equinox";
+            myCar.Year = 2015;
+
+            var teenCar = new Car()
+            {
+                Make = "Ford",
+                Model = "Focus",
+                Year = 2003,
+            };
+
+            var wifeCar = new Car("Chevy", "Trailblazer", 2022);
+
+            var carList = new List<Car>() { myCar, teenCar, wifeCar };
+
+            foreach(var car in carList)
+            {
+                Console.WriteLine($" {car.Year} {car.Make} {car.Model}");
+            }
+
         }
     }
 }
